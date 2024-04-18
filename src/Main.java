6576b95dacf08296ca88baade4ee1bb412fb1bca
nikce <git@github.com:kolya-shevtzov/helloworld.git>
2024-04-18 15:50:23 +0300

    cycle2

--- src/Main.java
+++ src/Main.java
@@ -31,12 +31,11 @@ public class Main {
         }
          double total1=0;
         double salary1=15000;
-        double c=0.07;
         int d=0;
 
         for (;total1<12_000_000;d++)
         {
-            total1=total1+salary1*c;
+            total1=total1+salary1*1.07;
             System.out.println(d+" "+total1);
         }
         double total2=0;
@@ -44,20 +43,19 @@ public class Main {
         int d2=0;
         for(;total2<12_000_000;d2++)
         {
-            total2=total2+salary2*c;
+            total2=total2+salary2*1.07;
             if(d2%6==0)
                 System.out.println(d2+" "+total2);
         }
 
-        int total3=0;
-        int salary3=15000;
-        int b3=15000%7;
+        double total3=0;
+        double salary3=15000;
         int mounth=0;
         int YearMounth=12*9;
 
         for (;mounth<YearMounth;mounth++)
         {
-            salary3=salary3+b3;
+            salary3=salary3*1.07;
             total3=total3+salary3;
             if(mounth%6==0)
             {
@@ -69,13 +67,12 @@ public class Main {
         for(;friday<=31;friday+=7)
         {
             System.out.println("Сегодня пятница,"+friday+"-е число. Необходимо подготовить отчет");}
-        int currentYear = 2023;
-        for(int m=0;m<currentYear+100; m=m+79)
-    {
-        if (m>currentYear-200)
-        {
-            System.out.println(m);
+        int yaer1 = 2024 - 200;
+        int year2 = 2024 + 100;
+
+        for (int comet = yaer1; comet < year2; comet ++) {
+            if (comet % 79 == 0){
+                System.out.println(comet);}
         }
-    }
     }
 }
